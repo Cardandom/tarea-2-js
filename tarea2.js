23fcf873bea7ff31a7729872a2eeda052e5feb06
Aux inventario <auxinventario@grupoideasa.local>
2022-08-22 18:56:11 +0200

    Tarea 2 openbootcamps js con node

---
+++ tarea2.js
@@ -0,0 +1,11 @@
+const nombre = "Carlos Dominguez";
+var edad = 31;
+var desarrollador = true
+const fechaNacimiento = new Date(1991,3,8)
+let libroFav = {
+    titulo:"El ultimo exorcista",
+    autor:"gabrielle amort",
+    año : 2010,
+    URL : https://es.wikipedia.org/wiki/El_%C3%BAltimo_exorcismo,
+}
+
